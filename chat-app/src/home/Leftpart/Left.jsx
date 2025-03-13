e1eb3dc50a6b862e06674a38ccdeb84318845694
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-03-13 22:06:05 +0530

    update commit

--- chat-app/src/home/Leftpart/Left.jsx
+++ chat-app/src/home/Leftpart/Left.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Search from './Search'
 import Users from './Users'
+import Logout from './Logout'
 
 function Left() {
   return (
@@ -9,6 +10,7 @@ function Left() {
     <div className="w-[50%] text-gray-300 bg-gray-900"></div>
     <Search />
     <Users />
+    <Logout />
     </div>
     </>
   )
