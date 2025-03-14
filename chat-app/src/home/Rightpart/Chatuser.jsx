b9446f8bacdb12505d5ed06a347af525db74fc81
Avinash kushawaha <Mauryaavinash085@gmail.com>
2025-03-14 22:52:36 +0530

    update commit

---
+++ chat-app/src/home/Rightpart/Chatuser.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import useConversation from "../../zustand/useConversation.js";
+import { useSocketContext } from "../../context/SocketContext.jsx";
+import { CiMenuFries } from "react-icons/ci";
+import profile from "../../../public/user.jpg";
+function Chatuser() {
+  const { selectedConversation } = useConversation();
+  const { onlineUsers } = useSocketContext();
+  const getOnlineUsersStatus = (userId) => {
+    return onlineUsers.includes(userId) ? "Online" : "Offline";
+  };
